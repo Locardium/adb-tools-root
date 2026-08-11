d73ed0a2f70c201ced67eb674fb15f66a2ab71f6
Locos <guillermosaracini3@gmail.com>
2026-08-10 22:55:32 -0300

    Fix KsuWebUI: Extract webroot manually in customize.sh

--- customize.sh
+++ customize.sh
@@ -1,6 +1,10 @@
 #!/system/bin/sh
 # Magisk module installer script
 
+ui_print "- Extracting WebUI files..."
+unzip -o "$ZIPFILE" 'webroot/*' -d $MODPATH >&2
+unzip -o "$ZIPFILE" 'action.sh' -d $MODPATH >&2
+
 # Set permissions for scripts
 set_perm_recursive $MODPATH/webroot/cgi-bin 0 0 0755 0755
 set_perm $MODPATH/action.sh 0 0 0755
